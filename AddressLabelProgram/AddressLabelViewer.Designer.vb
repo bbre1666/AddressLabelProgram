9a7dcaf41b1e3aec4d152230b32251c29541936e
bbre1666 <badenbrenner@isu.edu>
2023-03-06 08:38:30 -0700

    Add project files.

---
+++ AddressLabelProgram/AddressLabelViewer.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class AddressLabel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.StreetTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.LabelPreview = New System.Windows.Forms.GroupBox()
+        Me.NirstName = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PerviewLableButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(119, 39)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(189, 27)
+        Me.FirstNameTextBox.TabIndex = 0
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(119, 83)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(189, 27)
+        Me.LastNameTextBox.TabIndex = 1
+        '
+        'StreetTextBox
+        '
+        Me.StreetTextBox.Location = New System.Drawing.Point(119, 132)
+        Me.StreetTextBox.Name = "StreetTextBox"
+        Me.StreetTextBox.Size = New System.Drawing.Size(189, 27)
+        Me.StreetTextBox.TabIndex = 2
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.Location = New System.Drawing.Point(119, 180)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.Size = New System.Drawing.Size(189, 27)
+        Me.CityTextBox.TabIndex = 3
+        '
+        'StateTextBox
+        '
+        Me.StateTextBox.Location = New System.Drawing.Point(119, 229)
+        Me.StateTextBox.Name = "StateTextBox"
+        Me.StateTextBox.Size = New System.Drawing.Size(189, 27)
+        Me.StateTextBox.TabIndex = 4
+        '
+        'ZipCodeTextBox
+        '
+        Me.ZipCodeTextBox.Location = New System.Drawing.Point(119, 282)
+        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
+        Me.ZipCodeTextBox.Size = New System.Drawing.Size(189, 27)
+        Me.ZipCodeTextBox.TabIndex = 5
+        '
+        'LabelPreview
+        '
+        Me.LabelPreview.Location = New System.Drawing.Point(429, 30)
+        Me.LabelPreview.Name = "LabelPreview"
+        Me.LabelPreview.Size = New System.Drawing.Size(345, 281)
+        Me.LabelPreview.TabIndex = 6
+        Me.LabelPreview.TabStop = False
+        Me.LabelPreview.Text = "LabelPreview"
+        '
+        'NirstName
+        '
+        Me.NirstName.AutoSize = True
+        Me.NirstName.Location = New System.Drawing.Point(33, 42)
+        Me.NirstName.Name = "NirstName"
+        Me.NirstName.Size = New System.Drawing.Size(80, 20)
+        Me.NirstName.TabIndex = 7
+        Me.NirstName.Text = "Frist Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Last Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 135)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Street Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(60, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "City"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(60, 232)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "State"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(42, 285)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 20)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Zip Code"
+        '
+        'PerviewLableButton
+        '
+        Me.PerviewLableButton.Location = New System.Drawing.Point(472, 378)
+        Me.PerviewLableButton.Name = "PerviewLableButton"
+        Me.PerviewLableButton.Size = New System.Drawing.Size(100, 51)
+        Me.PerviewLableButton.TabIndex = 13
+        Me.PerviewLableButton.Text = "Preview Label"
+        Me.PerviewLableButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(578, 378)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(100, 51)
+        Me.ClearButton.TabIndex = 14
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(684, 378)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(100, 51)
+        Me.ExitButton.TabIndex = 15
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'AddressLabel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.PerviewLableButton)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NirstName)
+        Me.Controls.Add(Me.LabelPreview)
+        Me.Controls.Add(Me.ZipCodeTextBox)
+        Me.Controls.Add(Me.StateTextBox)
+        Me.Controls.Add(Me.CityTextBox)
+        Me.Controls.Add(Me.StreetTextBox)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Name = "AddressLabel"
+        Me.Text = "AddressLabel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents StreetTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents ZipCodeTextBox As TextBox
+    Friend WithEvents LabelPreview As GroupBox
+    Friend WithEvents NirstName As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PerviewLableButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ AddressLabelProgram/AddressLabelViewer.vb
@@ -0,0 +1,114 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.IO.Compression
+
+Public Class AddressLabel
+    Function ValidInput() As Boolean
+        Dim allValid As Boolean = True
+        Dim zip As Integer
+        Try
+            zip = CInt(ZipCodeTextBox.Text)
+        Catch ex As Exception
+            AccumulateMessage("Zip Code must be a number")
+            ZipCodeTextBox.Focus()
+            allValid = False
+        End Try
+
+        If StateTextBox.Text = "" Then
+            AccumulateMessage("State must be entered")
+            StateTextBox.Focus()
+            allValid = False
+        End If
+
+        If CityTextBox.Text = "" Then
+            AccumulateMessage("City must be entered")
+            CityTextBox.Focus()
+            allValid = False
+        End If
+
+        If StreetTextBox.Text = "" Then
+            AccumulateMessage("Street name must be entered")
+            StreetTextBox.Focus()
+            allValid = False
+        End If
+
+        If LastNameTextBox.Text = "" Then
+            AccumulateMessage("Last name must be entered")
+            LastNameTextBox.Focus()
+            allValid = False
+        End If
+
+        If FirstNameTextBox.Text = "" Then
+            AccumulateMessage("First name must be entered")
+            FirstNameTextBox.Focus()
+            allValid = False
+        End If
+
+        If AccumulateMessage() <> "" Then
+            MsgBox(AccumulateMessage())
+            AccumulateMessage(, True)
+            allValid = False
+        End If
+        Return allValid
+    End Function
+
+    Private Function AccumulateMessage(Optional newMessage As String = "", Optional clear As Boolean = False) As String
+        Static _message As String
+        Select Case clear
+            Case False
+                If newMessage <> "" Then
+                    _message &= newMessage & vbCrLf
+                End If
+            Case Else
+                _message = ""
+        End Select
+        Return _message
+
+    End Function
+
+    Private Function Summary(Optional addRecord As Boolean = True) As String
+        Static _summary As String
+        'example of output
+        'First last
+        'Street Address
+        'City State Zip code
+        If addRecord Then
+            _summary &= $"{FirstNameTextBox.Text} {LastNameTextBox.Text}" & vbNewLine
+            _summary &= $"{StreetTextBox.Text}" & vbNewLine
+            _summary &= $"{CityTextBox.Text}" & "," & $" {StateTextBox.Text} {ZipCodeTextBox.Text}"
+        End If
+        Return _summary
+    End Function
+
+    Private Sub Clear()
+        'resete to dafalt 
+        FirstNameTextBox.Text = ""
+        LastNameTextBox.Text = ""
+        StreetTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipCodeTextBox.Text = ""
+        LabelPreview.Text = ""
+        'clears the stored values 
+        AccumulateMessage(, True)
+    End Sub
+
+    Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles PerviewLableButton.Click
+        'if the imput is valid thatn the lable will display
+        If ValidInput() Then
+            Summary()
+            Clear()
+        End If
+        LabelPreview.Text = Summary(False)
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        Clear()
+    End Sub
+End Class
+
